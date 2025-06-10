48f83fb7d9d1588e7646b8deb1b27903aad249c7
Muhammad-Zunair-Khalid <bscs24097@itu.edu.pk>
2025-06-10 21:34:44 +0500

    Create  main.cpp using RayLib library

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include "raylib.h"
+#include "Game.h"
+
+
+int main() {
+	Color grey = { 30,30,30,255 };
+	Color yellow = { 243,216,63,255 };
+	int offset = 50;
+	int WindowHeight = 750;
+	int WindowWidth = 750;
+	
+	InitWindow(WindowWidth + offset, WindowHeight+offset, "SpaceShip-Battle");
+	SetTargetFPS(60);
+
+	Game game;
+
+
+	while (WindowShouldClose() == false) {
+
+		BeginDrawing();
+		ClearBackground(grey);
+		DrawRectangleRoundedLines( { 10, 10, 788, 788 }, 0.18f, 20,yellow);
+
+	
+
+		EndDrawing();
+	}
+	CloseWindow();
+}
