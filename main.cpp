04190b4b532a91163584dcc06ead1821557bc924
Muhammad-Zunair-Khalid <bscs24097@itu.edu.pk>
2025-06-15 01:17:56 +0500

    Final Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,37 @@
 #include "raylib.h"
 #include "Game.h"
 
-
 int main() {
-	// UsingtheLibraryof Raylib
-	Color grey = { 30,30,30,255 };
-	Color yellow = { 243,216,63,255 };
-	int offset = 50;
-	int WindowHeight = 750;
-	int WindowWidth = 750;
-	
-	InitWindow(WindowWidth + offset, WindowHeight+offset, "SpaceShip-Battle");
-	SetTargetFPS(60);
+    Color grey = { 30,30,30,255 };
+    Color yellow = { 243,216,63,255 };
+    int offset = 50;
+    int WindowHeight = 750;
+    int WindowWidth = 750;
 
-	Game game;
+    InitWindow(WindowWidth + offset, WindowHeight + offset, "SpaceShip-Battle");
+    InitAudioDevice();
+    SetTargetFPS(60);
+
+    Game game;
+    
+   
 
 
-	while (WindowShouldClose() == false) {
+    while (WindowShouldClose() == false) {
+        UpdateMusicStream(game.music);
+        game.HandleInput();
+        game.Update();
 
-		BeginDrawing();
-		ClearBackground(grey);
-		DrawRectangleRoundedLines( { 10, 10, 788, 788 }, 0.18f, 20,yellow);
+        BeginDrawing();
+        ClearBackground(grey);
+        DrawRectangleRoundedLines({ 10, 10, 788, 788 }, 0.18f, 20, yellow);
+       
 
-	
-
-		EndDrawing();
-	}
-	CloseWindow();
+        game.Draw();
+        
+        EndDrawing();
+    }
+    CloseWindow();
+    CloseAudioDevice();
+    return 0;
 }
